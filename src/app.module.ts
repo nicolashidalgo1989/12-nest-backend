7010256229fe7843d8a915a855730ba3454cfa1d
Nicolas Hidalgo <38006165+nicolashidalgo1989@users.noreply.github.com>
2024-02-18 16:20:25 -0300

    fix: mongo uri

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { AuthModule } from './auth/auth.module';
 
+const MONGO_URI: string = process.env.MONGO_URI!;
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
     // MongooseModule.forRoot('mongodb://mongo:53435e1HFA-25BB215B3D3aCfC6FCC5c@roundhouse.proxy.rlwy.net:11611',{
     //   dbName: 'auth-db',
     // }),
-    MongooseModule.forRoot(process.env.MONGO_URI, {
+    MongooseModule.forRoot(MONGO_URI, {
     dbName: process.env.MONGO_DB_NAME,
     }),
     AuthModule,
